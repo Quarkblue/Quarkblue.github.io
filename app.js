2ba8710d339439a745503eb62bd046071e95a3b9
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-24 21:40:41 +0530

    Switching between sections

--- app.js
+++ app.js
@@ -8,10 +8,28 @@ function PageTransitions(){
     for(let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
+            currentBtn[0].className = currentBtn[0].className.replace(' active-btn','');
             this.className += ' active-btn';
         });
     }
-}
 
+    //Sections active class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            // remove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+
+            // Hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active');
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
 PageTransitions();
